d52c9fbf9129a3df1b62580848a87df046563550
Andrew Stonecipher <andrew.stonecipher@hotmail.com>
2016-02-21 21:40:35 -0600

    Allow for appointment time to pass through to api.

--- api.php
+++ api.php
@@ -10,7 +10,8 @@ switch ($action) {
 //         break;
         $name = $_POST['name'];
         $phone = $_POST['phone'];
-        echo setAppointment($name, $phone);
+        $appTime = $_POST['appTime'];
+        echo setAppointment($name, $phone, $appTime);
         break;
     default:
         echo "none";
@@ -35,11 +36,14 @@ function getSchedule($date)
             if( $hour > 11 ){
                 $amORpm = 'pm';
             }
+            
+            $idTime = "{$date}_{$hour}_{$v}";
+            
             $time = "{$hour}_{$v}";
             $out .= "<div id='{$date}_{$hour}_{$v}'>
                     $hour:$v $amORpm
                     <span class='{$time}_name'></span>
-                    <span class='glyphicon glyphicon-plus time_slot_add' id='$time' style='float: right; color: green;'></span>
+                    <span class='glyphicon glyphicon-plus time_slot_add' id='$idTime' style='float: right; color: green;'></span>
                     <hr>
                     </div>";
     }
@@ -51,8 +55,13 @@ function getSchedule($date)
     return $out;
 }
 
-function setAppointment($name, $phone){
-    $str = "Received $name : $phone<br>";
+function setAppointment($name, $phone, $appTime){
+    $parts = explode(":", $appTime);
+    $dateParts = explode('-', $parts[0] );
+    $time = explode("_", $parts[1]);
+    $date = date('l, F d, Y g:i a', mktime( $time[0], $time[1],0,$dateParts[1],$dateParts[2], $dateParts[0]));
+    
+    $str = "Received an appointment for $date , for<br>$name : $phone<br>";
     $str .= "Functionality still being worked on.";
     return $str;
 }
